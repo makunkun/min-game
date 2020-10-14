59dd8b9ff375c8af5b17ba686d665ee093f68bf2
马勇坤 <15191310018@163.com>
2020-10-14 17:54:25 +0800

    精灵类初始化

--- Main.js
+++ Main.js
@@ -1,15 +1,34 @@
 // 初始化整个游戏的精灵，作为游戏开始的入口
 import { ResourcesLoader } from "./js/base/ResourcesLoader.js";
+import { Director } from "./js/Director.js";
 
 export class Main {
   constructor() {
-    console.log('我执行啦');
     this.canvas = document.getElementById('game_canvas');
     this.ctx = this.canvas.getContext('2d');
+    // 资源加载器及获取回调的map对象
     const loader = ResourcesLoader.create();
     loader.onLoaded((map) => {
       this.onResourceFirstLoaded(map);
     })
+    // 导演单例
+    Director.getInstance();
+    // 测试
+    let image = new Image();
+    image.src='../res/background.png';
+    image.onload=() => {
+      this.ctx.drawImage(
+        image,
+        0,
+        0,
+        image.width,
+        image.height,
+        0,
+        0,
+        image.width,
+        image.height,
+      );
+    }
   }
   
   onResourceFirstLoaded(map) {
--- js/Director.js
+++ js/Director.js
@@ -1,5 +1,9 @@
 // 导演类，控制游戏的逻辑
 export class Director {
+  constructor() {
+    console.log('构造器初始化');
+  }
+  // 单例模式
   static getInstance() {
     if (!Director.instance) {
       Director.instance = new Director();
