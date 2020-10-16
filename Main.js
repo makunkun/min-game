f5716d48e671e2806d434ed4dc48e8e2c1779a69
马勇坤 <15191310018@163.com>
2020-10-16 15:39:37 +0800

    基础精灵类的封装；资源管理器的封装；导演单例控制游戏运行

--- Main.js
+++ Main.js
@@ -1,37 +1,45 @@
 // 初始化整个游戏的精灵，作为游戏开始的入口
-import { ResourcesLoader } from "./js/base/ResourcesLoader.js";
+import { ResourcesLoader } from './js/base/ResourcesLoader.js';
+import { BackGround } from './js/runtime/BackGround.js';
 import { Director } from "./js/Director.js";
+import { DataStore } from './js/base/DataStore.js';
 
 export class Main {
   constructor() {
     this.canvas = document.getElementById('game_canvas');
     this.ctx = this.canvas.getContext('2d');
+    // 初始化单例全局数据DataStore
+    this.dataStore = DataStore.getInstance();
     // 资源加载器及获取回调的map对象
     const loader = ResourcesLoader.create();
     loader.onLoaded((map) => {
       this.onResourceFirstLoaded(map);
     })
     // 导演单例
-    Director.getInstance();
-    // 测试
-    let image = new Image();
-    image.src='../res/background.png';
-    image.onload=() => {
-      this.ctx.drawImage(
-        image,
-        0,
-        0,
-        image.width,
-        image.height,
-        0,
-        0,
-        image.width,
-        image.height,
-      );
-    }
+    // Director.getInstance();
   }
   
+
+  // 第一次加载，获取到资源全部加载完成的map对象
   onResourceFirstLoaded(map) {
-    console.log(map);
+    // 不需要销毁的变量进行直接进行赋值，不放在map中（否则会被销毁）
+    // 放入画笔
+    this.dataStore.ctx = this.ctx;
+    // 放入所有图片组成的map对象
+    this.dataStore.res = map;
+    // 初始化精灵类
+    this.init();
   }
+  
+  // 初始化
+  init() {
+    // 向dataStore的map中 放入 background实例对象
+    this.dataStore
+      .put('background',
+        new BackGround(this.ctx,
+           this.dataStore.res.get('background')));
+    // 用单例导演 开始绘制
+    Director.getInstance().run();
+  }
+
 }
--- js/Director.js
+++ js/Director.js
@@ -1,8 +1,20 @@
+import { DataStore } from "./base/DataStore.js";
+
 // 导演类，控制游戏的逻辑
 export class Director {
   constructor() {
     console.log('构造器初始化');
+    this.dataStore = DataStore.getInstance();
   }
+  
+  // 游戏运行的方法
+  run() {
+    // 获取dataStore中的map对象的背景图实例background
+    const backgroundSprite = this.dataStore.get('background');
+    // 并调用BackGround类所继承的Sprite的draw方法进行绘制
+    backgroundSprite.draw();
+  }
+
   // 单例模式
   static getInstance() {
     if (!Director.instance) {
