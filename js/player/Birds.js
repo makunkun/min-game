787a46732abefd214bb3880da92e741b27185428
马勇坤 <15191310018@163.com>
2020-10-26 17:15:46 +0800

    初始化小鸟绘制变量

--- js/player/Birds.js
+++ js/player/Birds.js
@@ -9,6 +9,28 @@ export class Birds extends Sprite{
     super(image, 0, 0,
       image.width, image.height,
       0, 0, image.width, image.height);
+    // 小鸟的三种状态需要数组去存储
+    // 小鸟的宽是34，高是24，上下边距是10，左右边距是9。
+    this.clippingX = [
+      9,
+      9 + 34 + 18,
+      9 + 34 + 18 + 34
+    ];
+    this.clippingY = [10, 10, 10];
+    this.clippingWidth = [34, 34, 34];
+    this.clippingHeight = [24, 24, 24];
+    this.birdX = window.innerWidth / 4;
+    this.birdsX = [this.birdX, this.birdX, this.birdX];
+    this.birdY = window.innerHeight / 2;
+    this.birdsY = [this.birdY, this.birdY, this.birdY];
+    this.birdWidth = 34;
+    this.birdsWidth = [this.birdWidth, this.birdWidth, this.birdWidth];
+    this.birdHeight = 24;
+    this.birdsHeight = [this.birdHeight, this.birdHeight, this.birdHeight];
+    this.y = [this.birdY, this.birdY, this.birdY];
+    this.index = 0;
+    this.count = 0;
+    this.time = 0;
   }
 
 }
